83f344f02ca42ed0c615517a67dc5a48e3f63286
Luka Cheuk <winglok0416@gmail.com>
2023-04-03 20:51:31 +0800

    options for chrome

--- scrapping.py
+++ scrapping.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
@@ -11,8 +12,12 @@ from exception import StreamingDataNotAvailableException
 
 class ScrappingDriver:
     def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-extensions")
         self.url = "http://www.nasdaq.com/market-activity/stocks/aapl/latest-real-time-trades"
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         self.driver.get(self.url)
         self.first_visit = True
 
@@ -23,7 +28,7 @@ class ScrappingDriver:
             else:
                 self.first_visit = False
 
-            table_body = WebDriverWait(self.driver, 60).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "latest-real-time-trades__table-body")))
+            table_body = WebDriverWait(driver=self.driver, timeout=45, poll_frequency=1000).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "latest-real-time-trades__table-body")))
 
             return table_body.find_elements(By.CLASS_NAME, "latest-real-time-trades__row")
 
